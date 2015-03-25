4669601baf8e8d11d7bef6bd3b2ede2fe5e7dd8c
WinMac32 <seth@viaware.ca>
2015-03-25 17:26:03 -0600

    Added GPLv3 since I'll probably open source this eventually

--- src/main/java/ca/viaware/dlna/library/model/LibraryEntry.java
+++ src/main/java/ca/viaware/dlna/library/model/LibraryEntry.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2015 Seth Traverse
+ *
+ * This file is part of ViaWareDLNAServer.
+ *
+ * ViaWareDLNAServer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ViaWareDLNAServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ViaWareDLNAServer. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ca.viaware.dlna.library.model;
 
 import ca.viaware.api.sql.factory.obj.DatabaseObject;
